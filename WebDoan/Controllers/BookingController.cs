3931a51183b9c323758fb30083dad74ff9afc1f3
Hiephuynh200 <huynhhiepvan1998@gmail.com>
2023-04-26 10:07:14 +0700

    Database

---
+++ WebDoan/Controllers/BookingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDoan.Models;
+
+namespace WebDoan.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: Booking
+        myDataContextDataContext db = new myDataContextDataContext();
+
+        public ActionResult Index()
+        {
+            ViewBag.TenNV = new SelectList(db.NHANVIENs, "MaNV", "TenNV");
+            ViewBag.TenDV = new SelectList(db.DICHVUs, "MaDV", "TenDV");
+            return View();
+        }
+
+        public ActionResult Index1()
+        {
+            ViewBag.TenDV = new SelectList(db.DICHVUs, "MaDV", "TenDV");
+            return View();
+        }
+    }
+}
