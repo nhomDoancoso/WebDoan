ea1661c80f082b30f4d35c73cac32c1e9c97191c
Hiephuynh200 <huynhhiepvan1998@gmail.com>
2023-05-11 11:35:27 +0700

    kiẹt ngu

--- WebDoan/Controllers/BookingController.cs
+++ WebDoan/Controllers/BookingController.cs
@@ -21,45 +21,100 @@ namespace WebDoan.Controllers
             ViewBag.TenDV = new SelectList(db.DICHVUs, "MaDV", "TenDV");
             ViewBag.MaCN = new SelectList(db.CHINHANHs, "MaCN", "DiaChi");
             ViewBag.MaKH = new SelectList(db.KHACHHANGs, "MaKH", "TenKH");
+            ViewBag.Lich = new SelectList(db.Liches, "MaTime", "Time");
+
             return View();
         }
         [HttpPost]
-        public ActionResult Index(FormCollection collection, PHIEUDAT pd , Lich lich)
+        public ActionResult index(FormCollection collection, PHIEUDAT pd)
         {
-            var MaPD = collection["MaPD"];
-            var MAKH = collection["MaKH"];
-            var MaNV = collection["MaNV"];
-            var TimeLap = collection["TimeLap"];
-            var TimeHen = collection["TimeHen"];
-            var MaCn = collection["MaCN"];
-            var tenkh = collection["TenKH"];
-            var sdt = collection["SDT"];
-
-            var checkPD = db.PHIEUDATs.FirstOrDefault(x => x.MaPD.ToString() == MaPD);
+            var mapd = collection["mapd"];
+            var makh = collection["makh"];
+            var manv = collection["manv"];
+            var timelap = collection["timelap"];
+            var timehen = collection["timehen"];
+            var macn = collection["macn"];
+            var tenkh = collection["tenkh"];
+            var sdt = collection["sdt"];
+            pd.MaNV = int.Parse(manv);
+            pd.TimeLap = DateTime.Parse(timehen);
+            pd.TimeHen = DateTime.ParseExact(timelap, "dd/MM/yyyy hh:mm:ss", null);
+            pd.MaCN = int.Parse(macn);
+            pd.TenKH = tenkh;
+            pd.SDT = sdt;
+            var checkpd = db.PHIEUDATs.FirstOrDefault(x => x.MaPD.ToString() == mapd);
             db.PHIEUDATs.InsertOnSubmit(pd);
             db.SubmitChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("index");
         }
-
-        public IActionResult MyAction()
+        //public ActionResult Index(FormCollection collection, PHIEUDAT pd, Lich lich)
+        //{
+        //    string connectionString = @"Data Source=HIEPHUYNHB279\SQLEXPRESS;Initial Catalog=QuanLyDatLichCatToc;Integrated Security=True";
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Open();
+        //        string sql = "SELECT * FROM PHIEUDAT JOIN Lich ON PHIEUDAT.MaPD = Lich.MaTime";
+        //        using (SqlCommand command = new SqlCommand(sql, connection))
+        //        {
+        //            using (SqlDataReader reader = command.ExecuteReader())
+        //            {
+        //                while (reader.Read())
+        //                {
+        //                    var MaPD = collection["MaPD"];
+        //                    var MAKH = collection["MaKH"];
+        //                    var MaNV = collection["MaNV"];
+        //                    var TimeLap = collection["TimeLap"];
+        //                    var TimeHen = collection["TimeHen"];
+        //                    var MaCn = collection["MaCN"];
+        //                    var tenkh = collection["TenKH"];
+        //                    var sdt = collection["SDT"];
+        //                    pd.MaNV = int.Parse(MaNV);
+        //                    pd.TimeLap = DateTime.Parse(TimeHen);
+        //                    pd.TimeHen = DateTime.Parse(TimeLap);
+        //                    pd.MaCN = int.Parse(MaCn);
+        //                    pd.TenKH = tenkh;
+        //                    pd.SDT = sdt;
+        //                    var checkPD = db.PHIEUDATs.FirstOrDefault(x => x.MaPD.ToString() == MaPD);
+        //                    db.PHIEUDATs.InsertOnSubmit(pd);
+        //                    db.SubmitChanges();
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return RedirectToAction("Index");
+        //}
+        [HttpPost]
+        public ActionResult GetThoiGian(int maTime)
         {
-            string connectionString = @"Data Source=HIEPHUYNHB279\SQLEXPRESS;Initial Catalog=QuanLyDatLichCatToc;Integrated Security=True" ;
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            var lich = db.Liches.FirstOrDefault(x => x.MaTime == maTime);
+            if (lich != null)
             {
-                connection.Open();
-                string sql = "SELECT * FROM PHIEUDAT JOIN Lich ON PHIEUDAT.MaCN = Lich.MaTime";
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            db.SubmitChanges();
-                        }
-                    }
-                }
+                return Content(lich.Time.ToString());
             }
-            return (IActionResult)View();
+            else
+            {
+                return Content("");
+            }
         }
+        //public IActionResult MyAction()
+        //{
+        //    string connectionString = @"Data Source=HIEPHUYNHB279\SQLEXPRESS;Initial Catalog=QuanLyDatLichCatToc;Integrated Security=True" ;
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Open();
+        //        string sql = "SELECT * FROM PHIEUDAT JOIN Lich ON PHIEUDAT.MaPD = Lich.MaTime";
+        //        using (SqlCommand command = new SqlCommand(sql, connection))
+        //        {
+        //            using (SqlDataReader reader = command.ExecuteReader())
+        //            {
+        //                while (reader.Read())
+        //                {
+        //                    //xử lí 
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return (IActionResult)View();
+        //}
     }
 }
