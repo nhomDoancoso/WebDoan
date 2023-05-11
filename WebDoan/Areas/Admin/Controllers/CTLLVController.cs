ee0f1cc221b8111ffa7a0a29ff934f067f255e82
Hiephuynh200 <huynhhiepvan1998@gmail.com>
2023-05-11 19:58:37 +0700

    CTLLV

---
+++ WebDoan/Areas/Admin/Controllers/CTLLVController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDoan.Models;
+
+namespace WebDoan.Areas.Admin.Controllers
+{
+    public class CTLLVController : Controller
+    {
+        myDataContextDataContext db = new myDataContextDataContext();
+
+        // GET: Admin/CTLLV
+        public ActionResult Index()
+        {
+            var lst = from ss in db.CTLLVs select ss;
+            return View(lst);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.MaNV = new SelectList(db.NHANVIENs, "MaNV", "TenNV");
+            ViewBag.MaLich = new SelectList(db.Liches, "MaLich", "GioLamViec");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, CTLLV cTLLV)
+        {
+            var maLich = collection["MaLich"];
+            var manv = collection["MaNV"];
+            var checkCombo = db.CTLLVs.FirstOrDefault(x => x.MaLich.ToString() == maLich);
+            db.CTLLVs.InsertOnSubmit(cTLLV);
+            db.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var D_DV = db.CTLLVs.First(m => m.MaLich == id);
+            return View(D_DV);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            var D_DV = db.CTLLVs.Where(m => m.MaLich == id).First();
+            db.CTLLVs.DeleteOnSubmit(D_DV);
+            db.SubmitChanges();
+            return RedirectToAction("Index", "CTLLV");
+        }
+    }
+}
