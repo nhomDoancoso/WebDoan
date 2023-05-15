4a65921ae73714d5e2f6478bf24b59cf3a1abafe
Hiephuynh200 <huynhhiepvan1998@gmail.com>
2023-05-15 14:57:27 +0700

    hiephuynh: status -> done

--- WebDoan/Areas/Admin/Controllers/CTLLVController.cs
+++ WebDoan/Areas/Admin/Controllers/CTLLVController.cs
@@ -1,51 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using WebDoan.Models;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+//using WebDoan.Models;
 
-namespace WebDoan.Areas.Admin.Controllers
-{
-    public class CTLLVController : Controller
-    {
-        myDataContextDataContext db = new myDataContextDataContext();
+//namespace WebDoan.Areas.Admin.Controllers
+//{
+//    public class CTLLVController : Controller
+//    {
+//        myDataContextDataContext db = new myDataContextDataContext();
 
-        // GET: Admin/CTLLV
-        public ActionResult Index()
-        {
-            var lst = from ss in db.CTLLVs select ss;
-            return View(lst);
-        }
+//        // GET: Admin/CTLLV
+//        public ActionResult Index()
+//        {
+//            var lst = from ss in db.CTLLVs select ss;
+//            return View(lst);
+//        }
 
-        public ActionResult Create()
-        {
-            ViewBag.MaNV = new SelectList(db.NHANVIENs, "MaNV", "TenNV");
-            ViewBag.MaLich = new SelectList(db.Liches, "MaLich", "GioLamViec");
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Create(FormCollection collection, CTLLV cTLLV)
-        {
-            var maLich = collection["MaLich"];
-            var manv = collection["MaNV"];
-            var checkCombo = db.CTLLVs.FirstOrDefault(x => x.MaLich.ToString() == maLich);
-            db.CTLLVs.InsertOnSubmit(cTLLV);
-            db.SubmitChanges();
-            return RedirectToAction("Index");
-        }
-        public ActionResult Delete(int id)
-        {
-            var D_DV = db.CTLLVs.First(m => m.MaLich == id);
-            return View(D_DV);
-        }
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            var D_DV = db.CTLLVs.Where(m => m.MaLich == id).First();
-            db.CTLLVs.DeleteOnSubmit(D_DV);
-            db.SubmitChanges();
-            return RedirectToAction("Index", "CTLLV");
-        }
-    }
-}
+//        public ActionResult Create()
+//        {
+//            ViewBag.MaNV = new SelectList(db.NHANVIENs, "MaNV", "TenNV");
+//            ViewBag.MaLich = new SelectList(db.Liches, "MaLich", "GioLamViec");
+//            return View();
+//        }
+//        [HttpPost]
+//        public ActionResult Create(FormCollection collection, CTLLV cTLLV)
+//        {
+//            var maLich = collection["MaLich"];
+//            var manv = collection["MaNV"];
+//            var checkCombo = db.CTLLVs.FirstOrDefault(x => x.MaLich.ToString() == maLich);
+//            db.CTLLVs.InsertOnSubmit(cTLLV);
+//            db.SubmitChanges();
+//            return RedirectToAction("Index");
+//        }
+//        public ActionResult Delete(int id)
+//        {
+//            var D_DV = db.CTLLVs.First(m => m.MaLich == id);
+//            return View(D_DV);
+//        }
+//        [HttpPost]
+//        public ActionResult Delete(int id, FormCollection collection)
+//        {
+//            var D_DV = db.CTLLVs.Where(m => m.MaLich == id).First();
+//            db.CTLLVs.DeleteOnSubmit(D_DV);
+//            db.SubmitChanges();
+//            return RedirectToAction("Index", "CTLLV");
+//        }
+
+//        public ActionResult Edit(int id)
+//        {
+//            var lstCombo = db.CTLLVs.First(m => m.MaLich == id);
+//            return View(lstCombo);
+//        }
+
+//        [HttpPost]
+//        public ActionResult Edit(int id, FormCollection collection, CTLLV combo)
+//        {
+//            var E_Combo = db.CTLLVs.First(m => m.MaLich == id);
+//            var E_MaLich = collection["MaLich"];
+//            var E_MaNV = collection["MaNV"];
+
+//            E_Combo.MaLich = id;
+//            if (string.IsNullOrEmpty(E_MaLich))
+//            {
+//                ViewData["Error"] = "Don't empty!";
+//                return this.Create();
+//            }
+//            else
+//            {
+//                E_Combo.MaLich =Convert.ToInt32 (E_MaLich);
+//                E_Combo.MaNV = Convert.ToInt32(E_MaNV); 
+//                UpdateModel(E_Combo);
+//                db.SubmitChanges();
+//                return RedirectToAction("Index");
+//            }
+//            return this.Edit(id);
+//        }
+//    }
+//}
--- WebDoan/Areas/Admin/Controllers/ClientBookingController.cs
+++ WebDoan/Areas/Admin/Controllers/ClientBookingController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 using WebDoan.Models;
 
@@ -16,9 +15,49 @@ namespace WebDoan.Areas.Admin.Controllers
         public ActionResult Index()
         {
             var lst = from ss in db.PHIEUDATs select ss;
-            return View(lst);
+            return View(lst);   
         }
+        public ActionResult Confirm(int id)
+        {
+            var dbContext = new myDataContextDataContext();
+            var phieudat = dbContext.PHIEUDATs.Where(p => p.MaPD == id).FirstOrDefault();
 
+            if (phieudat != null)
+            {
+                phieudat.TrangThaiPhieuDat = !phieudat.TrangThaiPhieuDat; // toggle the TrangThaiPhieuDat property value
+                dbContext.SubmitChanges(); // save the changes to the database
+            }
 
+            // redirect to the same view that displays the item details
+            return RedirectToAction("Index", new { id = id });
+          
+        }
+        //[HttpPost]
+        //public ActionResult AddFavorite(int id)
+        //{
+        //    string result = "fail";
+        //    int userID = int.Parse(Session["UserID"].ToString());
+        //    Favorite favorite = context.Favorites.FirstOrDefault(f => f.UserID == userID && f.DishID == id);
+        //    if (favorite == null)
+        //    {
+
+        //        Favorite newFavor = new Favorite
+        //        {
+        //            DishID = id,
+        //            UserID = userID,
+        //        };
+        //        context.Favorites.Add(newFavor);
+        //        context.SaveChanges();
+        //        result = "fav";
+        //    }
+        //    else
+        //    {
+        //        context.Favorites.Remove(favorite);
+        //        context.SaveChanges();
+        //        result = "unfav";
+
+        //    }
+        //    return Json(result, JsonRequestBehavior.AllowGet);
+        //}
     }
 }
